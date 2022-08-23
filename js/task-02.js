71e20c4b1d9ac4698e12abcf5cf18b1f11456ff4
OlesyaY <103993684+OlesyaY@users.noreply.github.com>
2022-08-23 22:09:55 +0300

    fix task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,16 @@ const ingredients = [
   "Condiments"
 ]
 
+const liElements = []
 const ingredientsList = document.querySelector("#ingredients")
 
 for (let ingredient of ingredients) {
   const liItem = document.createElement("li")
 
+  liItem.className='item'
   liItem.textContent = ingredient
-  ingredientsList.append(liItem)
+
+  liElements.push(liItem)
 }
+
+ingredientsList.append(...liElements)
